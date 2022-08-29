0753ea676a1b39924cfe52c5399ab56542c6d2a3
Vicente Tejeda <vicente.tejeda@oliphantusa.com>
2022-08-29 17:38:42 -0400

    Task view added

--- tasks/models.py
+++ tasks/models.py
@@ -5,8 +5,14 @@ import datetime
 class Task(models.Model): #table
     task_name = models.CharField(max_length=200) #Field or column
     Task_despcription = models.CharField(max_length=500) #Field or column
+    Completed = models.BooleanField()
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return (self.task_name)
     
 class SubTask(models.Model):
-    subtask = models.ForeignKey(Task, on_delete=models.CASCADE)
+    task_asociated = models.ForeignKey(Task, on_delete=models.CASCADE)
     subtask_text = models.CharField(max_length=200)
+    def __str__(self):
+        return (self.subtask_text)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Task,SubTask
+from django.template import loader
 
 # Create your views here.
+def index (response):
+    Task_list = Task.objects.order_by('-pub_date')
+    template = loader.get_template('index.html')
+    context = {
+        'task_list': Task_list,
+    }
+    return HttpResponse(template.render(context, response))
