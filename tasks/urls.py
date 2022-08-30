97ae30fdf4a96ea14abb87a26336ba5c1c399c80
Vicente <vicente.tejeda@oliphantusa.com>
2022-08-30 16:47:12 -0400

    8/30/22 Views and database access

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
      path('', views.index,name="index"),
+     path('<int:task_id>/', views.task_detail, name='task_detail'),
+     path('form',views.task_form,name="task_form")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Task,SubTask
 from django.template import loader
+from django.urls import reverse
 
 # Create your views here.
 def index (response):
@@ -11,3 +12,19 @@ def index (response):
         'task_list': Task_list,
     }
     return HttpResponse(template.render(context, response))
+
+def task_detail(request,task_id):
+    task_ret = get_object_or_404(Task, pk=task_id)
+    template = loader.get_template('subtask.html')
+    subtask_list = task_ret.subtask_set.all()
+    context = {
+        'subtask_list': subtask_list,
+        'task':task_ret
+    }
+    return (HttpResponse(template.render(context, request)))
+
+def task_form(request):
+    template = loader.get_template('form.html')
+    return (HttpResponse(template.render()))
+    
+    
